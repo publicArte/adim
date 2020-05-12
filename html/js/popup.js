7e97387e075c5f3f33a7b48b401f528b01873e47
torhex137 <torhex137@gmail.com>
2020-05-11 19:04:24 -0600

    updates and added datafilter and popup

---
+++ html/js/popup.js
@@ -0,0 +1,29 @@
+//
+// Gets ID buttons add event listner and excutes based on id
+document.getElementById('portfoliolist').addEventListener('click', function(evt) {
+  var target = evt.target;
+
+  if (target.id === 'brand-1') {
+    document.getElementById('bPop-1').style.display = 'block';
+  }
+  /* else if (target.id === 'brand-2') {
+    document.getElementById('bPop-2').style.display = 'block';
+  }
+  else if (target.id === 'brand-3') {
+    document.getElementById('bPop-3').style.display = 'block';
+  }
+  else if (target.id === 'brand-4') {
+    document.getElementById('bPop-4').style.display = 'block';
+  }
+  else if (target.id === 'brand-5') {
+    document.getElementById('bPop-5').style.display = 'block';
+  }*/
+  else {
+    document.getElementById('bPop-1').style.display = 'none';
+    //document.getElementById('bPop-2').style.display = 'none';
+   // document.getElementById('bPop-3').style.display = 'none';
+   // document.getElementById('bPop-4').style.display = 'none';
+   // document.getElementById('bPop-5').style.display = 'none';
+  }
+}, false);
+
