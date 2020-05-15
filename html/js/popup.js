b22def6d7981a0b1d64bd5bc00426cb5165780b2
torhex137 <torhex137@gmail.com>
2020-05-15 14:00:31 -0600

    uodate

--- html/js/popup.js
+++ html/js/popup.js
@@ -15,15 +15,163 @@ document.getElementById('portfoliolist').addEventListener('click', function(evt)
   else if (target.id === 'brand-4') {
     document.getElementById('bPop-4').style.display = 'block';
   }
-  /*else if (target.id === 'brand-5') {
+  else if (target.id === 'brand-5') {
     document.getElementById('bPop-5').style.display = 'block';
-  }*/
+  }
+  else if (target.id === 'brand-6') {
+    document.getElementById('bPop-6').style.display = 'block';
+  }
+  else if (target.id === 'brand-7') {
+    document.getElementById('bPop-7').style.display = 'block';
+  }
+  else if (target.id === 'brand-8') {
+    document.getElementById('bPop-8').style.display = 'block';
+  }
+  else if (target.id === 'brand-9') {
+    document.getElementById('bPop-9').style.display = 'block';
+  }
+  else if (target.id === 'brand-10') {
+    document.getElementById('bPop-10').style.display = 'block';
+  }
+  else if (target.id === 'brand-11') {
+    document.getElementById('bPop-11').style.display = 'block';
+  }
+  else if (target.id === 'brand-12') {
+    document.getElementById('bPop-12').style.display = 'block';
+  }
+  else if (target.id === 'brand-13') {
+    document.getElementById('bPop-13').style.display = 'block';
+  }
+  else if (target.id === 'brand-14') {
+    document.getElementById('bPop-14').style.display = 'block';
+  }
+  else if (target.id === 'brand-15') {
+    document.getElementById('bPop-15').style.display = 'block';
+  }
+  else if (target.id === 'brand-16') {
+    document.getElementById('bPop-16').style.display = 'block';
+  }
+  else if (target.id === 'brand-17') {
+    document.getElementById('bPop-17').style.display = 'block';
+  }
+  else if (target.id === 'brand-18') {
+    document.getElementById('bPop-18').style.display = 'block';
+  }
+  else if (target.id === 'brand-19') {
+    document.getElementById('bPop-19').style.display = 'block';
+  }
+  else if (target.id === 'brand-20') {
+    document.getElementById('bPop-20').style.display = 'block';
+  }
+  else if (target.id === 'brand-21') {
+    document.getElementById('bPop-21').style.display = 'block';
+  }
+  else if (target.id === 'brand-22') {
+    document.getElementById('bPop-22').style.display = 'block';
+  }
+  else if (target.id === 'brand-23') {
+    document.getElementById('bPop-23').style.display = 'block';
+  }
+  else if (target.id === 'brand-24') {
+    document.getElementById('bPop-24').style.display = 'block';
+  }
+  else if (target.id === 'brand-25') {
+    document.getElementById('bPop-25').style.display = 'block';
+  }
+  else if (target.id === 'brand-26') {
+    document.getElementById('bPop-26').style.display = 'block';
+  }
+  else if (target.id === 'brand-27') {
+    document.getElementById('bPop-27').style.display = 'block';
+  }
+  else if (target.id === 'brand-28') {
+    document.getElementById('bPop-28').style.display = 'block';
+  }
+  else if (target.id === 'brand-29') {
+    document.getElementById('bPop-29').style.display = 'block';
+  }
+  else if (target.id === 'brand-30') {
+    document.getElementById('bPop-30').style.display = 'block';
+  }
+  else if (target.id === 'brand-31') {
+    document.getElementById('bPop-31').style.display = 'block';
+  }
+  else if (target.id === 'brand-32') {
+    document.getElementById('bPop-32').style.display = 'block';
+  }
+  else if (target.id === 'brand-33') {
+    document.getElementById('bPop-33').style.display = 'block';
+  }
+  else if (target.id === 'brand-34') {
+    document.getElementById('bPop-34').style.display = 'block';
+  }
+  else if (target.id === 'brand-35') {
+    document.getElementById('bPop-35').style.display = 'block';
+  }
+  else if (target.id === 'brand-36') {
+    document.getElementById('bPop-36').style.display = 'block';
+  }
+  else if (target.id === 'brand-37') {
+    document.getElementById('bPop-37').style.display = 'block';
+  }
+  else if (target.id === 'brand-38') {
+    document.getElementById('bPop-38').style.display = 'block';
+  }
+  else if (target.id === 'brand-39') {
+    document.getElementById('bPop-39').style.display = 'block';
+  }
+  else if (target.id === 'brand-40') {
+    document.getElementById('bPop-40').style.display = 'block';
+  }
+  else if (target.id === 'brand-41') {
+    document.getElementById('bPop-41').style.display = 'block';
+  }
+  else if (target.id === 'brand-42') {
+    document.getElementById('bPop-42').style.display = 'block';
+  }
   else {
     document.getElementById('bPop-1').style.display = 'none';
     document.getElementById('bPop-2').style.display = 'none';
     document.getElementById('bPop-3').style.display = 'none';
     document.getElementById('bPop-4').style.display = 'none';
-   // document.getElementById('bPop-5').style.display = 'none';
+    document.getElementById('bPop-5').style.display = 'none';
+    document.getElementById('bPop-6').style.display = 'none';
+    document.getElementById('bPop-7').style.display = 'none';
+    document.getElementById('bPop-8').style.display = 'none';
+    document.getElementById('bPop-9').style.display = 'none';
+    document.getElementById('bPop-10').style.display = 'none';
+    document.getElementById('bPop-11').style.display = 'none';
+    document.getElementById('bPop-12').style.display = 'none';
+    document.getElementById('bPop-13').style.display = 'none';
+    document.getElementById('bPop-14').style.display = 'none';
+    document.getElementById('bPop-15').style.display = 'none';
+    document.getElementById('bPop-16').style.display = 'none';
+    document.getElementById('bPop-17').style.display = 'none';
+    document.getElementById('bPop-18').style.display = 'none';
+    document.getElementById('bPop-19').style.display = 'none';
+    document.getElementById('bPop-20').style.display = 'none';
+    document.getElementById('bPop-21').style.display = 'none';
+    document.getElementById('bPop-22').style.display = 'none';
+    document.getElementById('bPop-23').style.display = 'none';
+    document.getElementById('bPop-24').style.display = 'none';
+    document.getElementById('bPop-25').style.display = 'none';
+    document.getElementById('bPop-26').style.display = 'none';
+    document.getElementById('bPop-27').style.display = 'none';
+    document.getElementById('bPop-28').style.display = 'none';
+    document.getElementById('bPop-29').style.display = 'none';
+    document.getElementById('bPop-30').style.display = 'none';
+    document.getElementById('bPop-31').style.display = 'none';
+    document.getElementById('bPop-32').style.display = 'none';
+    document.getElementById('bPop-33').style.display = 'none';
+    document.getElementById('bPop-34').style.display = 'none';
+    document.getElementById('bPop-35').style.display = 'none';
+    document.getElementById('bPop-36').style.display = 'none';
+    document.getElementById('bPop-37').style.display = 'none';
+    document.getElementById('bPop-38').style.display = 'none';
+    document.getElementById('bPop-39').style.display = 'none';
+    document.getElementById('bPop-40').style.display = 'none';
+    document.getElementById('bPop-41').style.display = 'none';
+    document.getElementById('bPop-42').style.display = 'none';
   }
 }, false);
 
