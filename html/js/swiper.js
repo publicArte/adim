2e14cd18aff53c5d56776e65a11bb5d14d5a3999
torhex137 <torhex137@gmail.com>
2020-05-28 23:26:38 -0600

    update

--- html/js/swiper.js
+++ html/js/swiper.js
@@ -2,7 +2,13 @@ $(function () {
 
   $("<div class='swipeView'></div>").appendTo("#gallery");
   
-  $("#gallery")
+  $("#gallery img.gallery_img").each(function() {
+    var el = $(this);
+    $("<img />", {
+      "class" : "swipe-img",
+      "src"   : el.attr("src")
+    }).appendTo("#gallery div.swipeView");
+  });
   
 
 });
