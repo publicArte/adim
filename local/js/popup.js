4e8c839686fead977ca7c3f8d79651a544d9b801
torhex137 <torhex137@gmail.com>
2020-06-04 23:11:49 -0600

    latest updates for  responsive and  functionality

--- local/js/popup.js
+++ local/js/popup.js
@@ -51,23 +51,11 @@ document.getElementById('portfoliolist').addEventListener('click', function(evt)
   else if (target.id === 'brand-16') {
     document.getElementById('bPop-16').style.display = 'block';
   }
-  else if (target.id === 'brand-17') {
-    document.getElementById('bPop-17').style.display = 'block';
-  }
-  else if (target.id === 'brand-18') {
-    document.getElementById('bPop-18').style.display = 'block';
-  }
-  else if (target.id === 'brand-19') {
-    document.getElementById('bPop-19').style.display = 'block';
-  }
   else if (target.id === 'brand-20') {
     document.getElementById('bPop-20').style.display = 'block';
   }
   else if (target.id === 'brand-21') {
     document.getElementById('bPop-21').style.display = 'block';
-  }
-  else if (target.id === 'brand-22') {
-    document.getElementById('bPop-22').style.display = 'block';
   }
   else if (target.id === 'brand-23') {
     document.getElementById('bPop-23').style.display = 'block';
@@ -81,9 +69,6 @@ document.getElementById('portfoliolist').addEventListener('click', function(evt)
   else if (target.id === 'brand-26') {
     document.getElementById('bPop-26').style.display = 'block';
   }
-  else if (target.id === 'brand-27') {
-    document.getElementById('bPop-27').style.display = 'block';
-  }
   else if (target.id === 'brand-28') {
     document.getElementById('bPop-28').style.display = 'block';
   }
@@ -92,9 +77,6 @@ document.getElementById('portfoliolist').addEventListener('click', function(evt)
   }
   else if (target.id === 'brand-30') {
     document.getElementById('bPop-30').style.display = 'block';
-  }
-  else if (target.id === 'brand-31') {
-    document.getElementById('bPop-31').style.display = 'block';
   }
   else if (target.id === 'brand-32') {
     document.getElementById('bPop-32').style.display = 'block';
@@ -120,9 +102,6 @@ document.getElementById('portfoliolist').addEventListener('click', function(evt)
   else if (target.id === 'brand-39') {
     document.getElementById('bPop-39').style.display = 'block';
   }
-  else if (target.id === 'brand-40') {
-    document.getElementById('bPop-40').style.display = 'block';
-  }
   else if (target.id === 'brand-41') {
     document.getElementById('bPop-41').style.display = 'block';
   }
@@ -146,21 +125,15 @@ document.getElementById('portfoliolist').addEventListener('click', function(evt)
     document.getElementById('bPop-14').style.display = 'none';
     document.getElementById('bPop-15').style.display = 'none';
     document.getElementById('bPop-16').style.display = 'none';
-    document.getElementById('bPop-17').style.display = 'none';
-    document.getElementById('bPop-18').style.display = 'none';
-    document.getElementById('bPop-19').style.display = 'none';
     document.getElementById('bPop-20').style.display = 'none';
     document.getElementById('bPop-21').style.display = 'none';
-    document.getElementById('bPop-22').style.display = 'none';
     document.getElementById('bPop-23').style.display = 'none';
     document.getElementById('bPop-24').style.display = 'none';
     document.getElementById('bPop-25').style.display = 'none';
     document.getElementById('bPop-26').style.display = 'none';
-    document.getElementById('bPop-27').style.display = 'none';
     document.getElementById('bPop-28').style.display = 'none';
     document.getElementById('bPop-29').style.display = 'none';
     document.getElementById('bPop-30').style.display = 'none';
-    document.getElementById('bPop-31').style.display = 'none';
     document.getElementById('bPop-32').style.display = 'none';
     document.getElementById('bPop-33').style.display = 'none';
     document.getElementById('bPop-34').style.display = 'none';
@@ -169,7 +142,6 @@ document.getElementById('portfoliolist').addEventListener('click', function(evt)
     document.getElementById('bPop-37').style.display = 'none';
     document.getElementById('bPop-38').style.display = 'none';
     document.getElementById('bPop-39').style.display = 'none';
-    document.getElementById('bPop-40').style.display = 'none';
     document.getElementById('bPop-41').style.display = 'none';
     document.getElementById('bPop-42').style.display = 'none';
   }
